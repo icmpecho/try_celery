12addad162abb0b340df4d46d10de3d12dcf96ac
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2015-06-10 13:34:52 +0700

    Connect to psql database

--- try_celery/settings.py
+++ try_celery/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'try_celery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'try_celery',
+        'USER': 'development',
+        'PASSWORD': 'development',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
