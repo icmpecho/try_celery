1bdd64f40c5a8e972b7022d73843bbcae35f3f59
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2015-06-10 11:43:00 +0700

    Add celery tasks

--- try_celery/settings.py
+++ try_celery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'try_celery'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery
+BROKER_URL = 'amqp://guest@localhost//'
